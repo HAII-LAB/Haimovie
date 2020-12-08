848e30e3ceee3d93ef887ebeeffb9c8bea1d85d6
HAII-LAB <seongminha0910@gmail.com>
2020-12-08 11:24:54 +0900

    add jsx stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+function Food({fav}){
+  return <h1>I like {fav}.</h1>;
+}
+
 function App() {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <h1>Hi!</h1>
+      <Food fav="Egg Bread"/>
+    </div>
+  );
 }
 
 export default App;
