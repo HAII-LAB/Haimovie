983ad6f792d75f25a1c0306eba083d26828e96ce
HAII-LAB <seongminha0910@gmail.com>
2020-12-09 19:03:24 +0900

    create object and map it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
-function Food({fav}){
-  return <h1>I like {fav}.</h1>;
+function Food({name, pic}){
+  return(
+    <div>
+      <h2>I like {name}</h2>
+      <img src={pic} />
+    </div>
+  )
+
 }
+
+const foodILike = [
+  {
+    name: "Egg Bread",
+    image: "http://www.maangchi.com/wp-content/uploads/2016/03/gyerangppang-cut.jpg"
+  },
+  {
+    name: "Hotdog",
+    image: "https://potatorolls.com/wp-content/uploads/Basic-Hot-Dogs.jpg"
+  }
+];
 
 function App() {
   return (
     <div>
       <h1>Hi!</h1>
-      <Food fav="Egg Bread"/>
+      {
+        foodILike.map(dish => 
+          <Food name={dish.name} pic={dish.image}/>
+        )
+      }
     </div>
   );
 }
